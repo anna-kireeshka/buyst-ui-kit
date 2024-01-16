c2eb31f3462264a677185a9513a1918390552eb2
Anna <77583972+anna-kireeshka@users.noreply.github.com>
2024-01-16 12:57:12 +0200

    build

---
+++ dist/Button/Button.stories.d.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Button from './Button';
+declare const _default: ComponentMeta<React.FC<import("./Button").MyButtonProps>>;
+export default _default;
+type StoryButton = ComponentStory<typeof Button>;
+export declare const Green: StoryButton;
+export declare const Red: StoryButton;
+export declare const Orange: StoryButton;
+export declare const Primary: StoryButton;
+export declare const Gray: StoryButton;
+export declare const Disabled: StoryButton;
+export declare const Default: StoryButton;
---
+++ dist/Input/Input.stories.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Input from './Input';
+declare const _default: ComponentMeta<React.FC<import("./Input").MyInputProps>>;
+export default _default;
+export declare const Text: ComponentStory<typeof Input>;
+export declare const Password: ComponentStory<typeof Input>;
+export declare const Number: ComponentStory<typeof Input>;
+export declare const Default: ComponentStory<React.FC<import("./Input").MyInputProps>>;
