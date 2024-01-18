48330e29782d6db81a55ab9cbb44af5552f99319
Anna <77583972+anna-kireeshka@users.noreply.github.com>
2024-01-18 14:32:35 +0200

    (18-01-2023) fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,37 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.ts',
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: "umd",
-        clean: true
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx']
-    },
-    externals: {
-        react: 'react'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: 'style-loader' },
-                    { loader: 'css-loader' },
-                ],
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    { loader: 'style-loader' },
-                    { loader: 'css-loader' },
-                    { loader: 'sass-loader' },
-                ],
-            },
+   mode: 'production',
+   entry: './src/index.ts',
+   output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'umd',
+      clean: true,
+      globalObject: 'this',
+   },
+   resolve: {
+      extensions: ['.ts', '.tsx'],
+   },
+   externals: {
+      react: 'react',
+   },
+   module: {
+      rules: [
+         {
+            test: /\.css$/,
+            use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+         },
+         {
+            test: /\.scss$/,
+            use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
+         },
 
-            {
-                test: /\.(ts|tsx)?$/,
-                use: ['ts-loader'],
-                exclude: /node_modules/
-            }
-        ],
-    },
+         {
+            test: /\.(ts|tsx)?$/,
+            use: ['ts-loader'],
+            exclude: /node_modules/,
+         },
+      ],
+   },
 }
