910ce70182c44490c950251cf0cb983a8ad01934
Anna <77583972+anna-kireeshka@users.noreply.github.com>
2024-01-19 09:40:09 +0200

    (19-01-2023) fix webpack build for ssr

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
       path: path.resolve(__dirname, 'dist'),
       libraryTarget: 'umd',
       clean: true,
+      globalObject: 'this',
    },
    resolve: {
       extensions: ['.ts', '.tsx'],
